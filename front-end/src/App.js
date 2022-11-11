422c5565c557e6ff7cbef355eda6661af35adb19
Alima Zhagufarova <az2180@nyu.edu>
2022-11-10 20:24:39 -0500

    -

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "../src/components/header/index";
-import ProjectCard from './components/projects/ProjectCard';
 import Footer from './components/footer';
 
 function App() {
