432805b1cdf552154b052c9fcb5532f5ea409290
Alima Zhagufarova <az2180@nyu.edu>
2022-11-21 02:19:21 -0500

    contacts beta version

--- front-end/src/components/contact/contact.js
+++ front-end/src/components/contact/contact.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./contact.css";
-
-function Contact(){
-    return(
-        <div className="contactsLogos">
-            
+import SocialContact from "./social-contact";
+function Contact() {
+  return (
+    <div className="contact">
+    <div className="contact-container">
+      <div className="contact-left">
+        <p>let's connect!</p>
+        <SocialContact />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
---
+++ front-end/src/components/data/socials.js
@@ -0,0 +1,27 @@
+export const SocialData=[
+    {
+        platform:"Telegram",
+        icon: require("../data/assets/telegram.png"),
+        link:"https://web.telegram.org",
+    },
+    {
+        platform:"Github",
+        icon: require("../data/assets/github.png"),
+        link:"https://github.com",
+    },
+    {
+        platform:"LinkedIn",
+        icon: require("../data/assets/linkedin.png"),
+        link:"https://www.youtube.com",
+    },
+    {
+        platform:"Twitter",
+        icon: require("../data/assets/twt.png"),
+        link: "https://twitter.com",
+    },
+    {
+        platform:"Facebook",
+        icon: require("../data/assets/linkedin.png"),
+        link:"https://www.facebook.com",
+    },
+];
